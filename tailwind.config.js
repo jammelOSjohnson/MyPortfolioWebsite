a267df0bb04d1470d784f15fddd8221321349e7f
jjohnson <jammel.johnson@myguardiangroup.com>
2021-10-08 13:44:44 -0500

    updating some configs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or false  or 'media'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
