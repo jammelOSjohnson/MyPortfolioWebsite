7330745a71edb02e5d0901c84de5539904bca295
jammelOSjohnson <jammeljohnson@gmail.com>
2022-04-11 14:01:56 -0500

    fix warnings and errors

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { HomeScreen } from '../Screens/Home/HomeScreen';
 import Counter from '../Screens/Counter';
 import TodoList from '../Screens/TodoList';
-import { Header } from './Header';
-import { Footer } from './Footer';
+//import { Header } from './Header';
+//import { Footer } from './Footer';
 //Import provider
 
 
